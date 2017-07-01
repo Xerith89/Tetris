6857fa704bdf9372121aa011a833e0927a55c713
paul <paul@PAULSLAPTOP>
2017-07-01 14:39:56 +0100

    Removed unneccessary funcs and vars from board and location

--- Engine/Board.h
+++ Engine/Board.h
@@ -4,16 +4,13 @@
 class Board
 {
 private:
-	static constexpr int cellDimension= 20;
-	static constexpr int callWidth = 20;
-	static constexpr int cellHeight = 20;
-	static constexpr int cellpadding = 2;
-	static constexpr int x_offset = 30;
-	static constexpr int y_offset = 30;
+
+	static constexpr int cellWidth = 10;
+	static constexpr int cellHeight = 10;
+	static constexpr int cellDimension = cellWidth+cellHeight;
 	Graphics& gfx;
 public:
 	void DrawCell(Location& loc, Color c); 
-	void DrawCellTwo(Location& loc, Color c);
 	void DrawWall(Color c);
 	Board(Graphics &gfx);
 };
